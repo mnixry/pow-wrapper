fae4550fd47b18f6a245003b6fd7e6822477c1c7
Mix <32300164+mnixry@users.noreply.github.com>
2024-02-13 17:02:03 +0800

    Add client.py for client impl

---
+++ client.py
@@ -0,0 +1,33 @@
+import hashlib
+import string
+import itertools
+
+import pwn
+import re
+
+
+
+def proof_of_work(nonce: str, difficulty: int):
+    nonce_byte = nonce.encode()
+    for salt in itertools.chain.from_iterable(
+        map(bytes, itertools.product((string.ascii_letters + string.digits).encode(), repeat=i))
+        for i in itertools.count(1)
+    ):
+        if (
+            hashlib.sha256(nonce_byte + salt).hexdigest()[:difficulty]
+            == "0" * difficulty
+        ):
+            return salt
+    raise ValueError("No solution found")
+
+p = pwn.remote('localhost', 1337)
+
+matched = re.search(
+    r"sha256\('(?P<nonce>[-A-Za-z0-9+/]+?)'.*?\)(?:.*?)startswith\('0'\s*\*\s*(?P<diff>\d+)\)",
+    p.recvuntil(b"== ").decode(),
+)
+assert matched, "No proof of work found"
+nonce = matched.group("nonce")
+difficulty = int(matched.group("diff"))
+p.send(proof_of_work(nonce, difficulty))
+p.interactive()
