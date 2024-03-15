6cced3854258441c07f6da1c4d2d5cbae60dd08a
Mix <32300164+mnixry@users.noreply.github.com>
2024-03-15 20:33:49 +0800

    Add command line argument parsing and improve proof of work algorithm

--- client.py
+++ client.py
@@ -3,7 +3,17 @@ import string
 import itertools
 
 from pwn import remote
+import sys
 import re
+
+
+program, *args = sys.argv
+if len(args) != 2:
+    print(f"Usage: {program} <target> <port>")
+    sys.exit(1)
+target, port = args
+
+p = remote(target, port)
 
 
 def proof_of_work(
@@ -12,7 +22,6 @@ def proof_of_work(
     salt_charset: str = string.ascii_letters + string.digits,
 ):
     nonce_byte = nonce.encode()
-    expected_prefix = "0" * difficulty
     for salt in itertools.chain.from_iterable(
         map(
             bytes,
@@ -20,17 +29,19 @@ def proof_of_work(
         )
         for i in itertools.count(1)
     ):
-        if hashlib.sha256(nonce_byte + salt).hexdigest().startswith(expected_prefix):
+        if (
+            int.from_bytes(hashlib.sha256(nonce_byte + salt).digest(), "big")
+            >> (256 - difficulty)
+            == 0
+        ):
             return salt
     raise ValueError("No solution found")
 
-
-p = remote("localhost", 1337)
-
 matched = re.search(
-    r"sha256\('(?P<nonce>[-A-Za-z0-9+/]+?)'.*?\)(?:.*?)startswith\('0'\s*\*\s*(?P<diff>\d+)\)",
-    p.recvuntil(b"== ").decode(),
+    r"nonce=(?P<nonce>.+?),\s*difficulty=(?P<diff>\d+)",
+    p.recvuntil(b"salt:").decode(),
 )
+
 assert matched, "No proof of work found"
 nonce = matched.group("nonce")
 difficulty = int(matched.group("diff"))
